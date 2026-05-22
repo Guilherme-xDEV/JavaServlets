0926905af4256dc450edbc8a7dba0ac938972916
guilherme <guilhermegm2020k@gmail.comclear>
2026-05-21 23:28:27 -0300

    Initial Structure & Form Example Of Servlet

---
+++ src/main/java/com/example/servlet/servlets/GreetServlet.java
@@ -0,0 +1,53 @@
+package com.example.servlet.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/greet")
+public class GreetServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(
+            HttpServletRequest req,
+            HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+
+        if (name == null || name.isBlank()) {
+            name = "Visitante";
+        }
+
+        if (email == null || email.isBlank()) {
+            email = "Não informado";
+        }
+
+        String htmlResponse = """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>Response</title>
+            </head>
+            <body>
+
+                <h1>Data received successfully!</h1>
+
+                <p>Name: %s</p>
+                <p>Email: %s</p>
+
+            </body>
+            </html>
+            """.formatted(name, email);
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        resp.getWriter().print(htmlResponse);
+    }
+}
